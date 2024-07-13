87a39d868b43df69bf4eb1324f00f07691acc730
Aldin Sakic <aldinsakic@duck.com>
2024-07-13 04:22:40 +0200

    added a self incrementing counter

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Counter from './components/Counter.vue'
 </script>
 
 <template>
@@ -14,6 +15,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
+    <Counter />
   </main>
 </template>
 
---
+++ vue-playground/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment(){
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<style scoped>
+  button{
+    background-color: #ba66ba;
+    border: none;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: x-large;
+  }
+</style>
